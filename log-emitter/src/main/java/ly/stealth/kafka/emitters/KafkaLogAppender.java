bebe927c7f1bd4a0e38325e1d58d44e517caffd2
serejja <serejja@gmail.com>
2015-04-28 15:28:02 +0300

    Added kafka log4j appender implementation, test

--- log-emitter/src/main/java/ly/stealth/kafka/emitters/KafkaLogAppender.java
+++ log-emitter/src/main/java/ly/stealth/kafka/emitters/KafkaLogAppender.java
@@ -1,16 +1,73 @@
 package ly.stealth.kafka.emitters;
 
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import ly.stealth.avro.LogLine;
+import org.apache.avro.generic.IndexedRecord;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.Level;
 import org.apache.log4j.spi.LoggingEvent;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
 public class KafkaLogAppender extends AppenderSkeleton {
+    public static final Long TraceLogTypeId = 1L;
+    public static final Long DebugLogTypeId = 2L;
+    public static final Long InfoLogTypeId = 3L;
+    public static final Long WarnLogTypeId = 4L;
+    public static final Long ErrorLogTypeId = 5L;
+    public static final Long FatalLogTypeId = 6L;
+
+    private static final Map<Level, Long> logLevels = new HashMap<Level, Long>();
+
+    static {
+        logLevels.put(Level.TRACE, TraceLogTypeId);
+        logLevels.put(Level.DEBUG, DebugLogTypeId);
+        logLevels.put(Level.INFO, InfoLogTypeId);
+        logLevels.put(Level.WARN, WarnLogTypeId);
+        logLevels.put(Level.ERROR, ErrorLogTypeId);
+        logLevels.put(Level.FATAL, FatalLogTypeId);
+    }
+
+    private KafkaProducer<String, IndexedRecord> producer;
+    private String topic;
+
+    public KafkaLogAppender(Properties props, String topic) {
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        this.producer = new KafkaProducer<String, IndexedRecord>(props);
+        this.topic = topic;
+    }
+
     @Override
     protected void append(LoggingEvent event) {
+        LogLine line = new LogLine();
+        line.setLine(event.getMessage() == null ? null : event.getMessage().toString());
+        line.setLogtypeid(logLevels.get(event.getLevel()));
+        line.setSource(event.getThreadName());
+
+        Map<CharSequence, Long> timings = new HashMap<CharSequence, Long>();
+        timings.put("emitted", event.getTimeStamp());
+
+        line.setTimings(timings);
+
+        Map<CharSequence, CharSequence> tags = new HashMap<CharSequence, CharSequence>();
+        tags.put("logger.name", event.getLoggerName());
+        tags.put("location", event.getLocationInformation().fullInfo);
+
+        line.setTag(tags);
+
+        ProducerRecord<String, IndexedRecord> record = new ProducerRecord<String, IndexedRecord>(this.topic, line);
+        producer.send(record);
     }
 
     @Override
     public void close() {
-
+        this.producer.close();
     }
 
     @Override
---
+++ log-emitter/src/test/java/ly/stealth/kafka/emitters/KafkaLogAppenderTest.java
@@ -0,0 +1,69 @@
+package ly.stealth.kafka.emitters;
+
+import io.confluent.kafka.serializers.KafkaAvroDecoder;
+import kafka.consumer.Consumer;
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.ConsumerIterator;
+import kafka.message.MessageAndMetadata;
+import kafka.serializer.StringDecoder;
+import kafka.utils.VerifiableProperties;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.log4j.Logger;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class KafkaLogAppenderTest {
+    private static String brokerList = "localhost:9092";
+    private static String zookeeper = "localhost:2181";
+    private static String schemaRegistry = "http://localhost:8081";
+    private static String topic = "kafka-log-appender-test-" + System.currentTimeMillis();
+
+    private static String loggerName = "logger-test";
+    private static Logger logger;
+
+    @BeforeClass
+    public static void initialize() {
+        logger = Logger.getLogger(loggerName);
+
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerList);
+        props.put("schema.registry.url", schemaRegistry);
+        logger.addAppender(new KafkaLogAppender(props, topic));
+    }
+
+    @Test
+    public void TestKafkaLogAppender() {
+        Properties consumerProps = new Properties();
+        consumerProps.put("zookeeper.connect", zookeeper);
+        consumerProps.put("group.id", "kafka-log-appender-test");
+        consumerProps.put("auto.offset.reset", "smallest");
+        consumerProps.put("schema.registry.url", schemaRegistry);
+        consumerProps.put("specific.avro.reader", "true");
+
+        Map<String, Integer> topicMap = new HashMap<String, Integer>();
+        topicMap.put(topic, 1);
+
+        ConsumerIterator<String, Object> iterator = Consumer.createJavaConsumerConnector(new ConsumerConfig(consumerProps))
+                .createMessageStreams(topicMap, new StringDecoder(null), new KafkaAvroDecoder(new VerifiableProperties(consumerProps)))
+                .get(topic).get(0).iterator();
+
+        String testMessage = "I am a test message";
+        logger.info(testMessage);
+
+        MessageAndMetadata<String, Object> messageAndMetadata = iterator.next();
+        GenericRecord logLine = (GenericRecord) messageAndMetadata.message();
+        assertEquals(logLine.get("line").toString(), testMessage);
+        assertEquals(logLine.get("logtypeid"), KafkaLogAppender.InfoLogTypeId);
+        assertNotNull(logLine.get("source"));
+        assertEquals(((Map<CharSequence, Object>) logLine.get("timings")).size(), 1);
+        assertEquals(((Map<CharSequence, Object>) logLine.get("tag")).size(), 2);
+    }
+}
